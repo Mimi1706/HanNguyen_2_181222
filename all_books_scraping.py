e14f70faf17403b932645e7fa7f7816aba377049
Mimi <hana.nguyen@outlook.fr>
2022-12-27 15:25:21 +0100

    Added comments

--- all_books_scraping.py
+++ all_books_scraping.py
@@ -7,7 +7,7 @@ def get_all_books_urls():
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser") ## parser type
 
-    total_pages = int(soup.find("li", {"class": "current"}).text.split()[-1])
+    total_pages = int(soup.find("li", {"class": "current"}).text.split()[-1]) ## total number of pages at the bottom of the page
     book_infos = soup.find_all("article", {"class": "product_pod"})
     books_urls = []
 
@@ -15,15 +15,16 @@ def get_all_books_urls():
         url = 'http://books.toscrape.com/catalogue/page-{0}.html'.format(page)
 
         for book in book_infos:
-            book_url = "http://books.toscrape.com/" + book.find("a")['href']
+            book_url = "http://books.toscrape.com/" + book.find("a")['href'] ## url of the book
             books_urls.append(book_url)
     
     return books_urls
 
 def get_book_infos(book_url):
-    response = requests.get(book_url)
-    soup = BeautifulSoup(response.text, "html.parser")
+    response = requests.get(book_url) ## get to the book url
+    soup = BeautifulSoup(response.text, "html.parser") 
 
+    ## retrieve the data
     product_page_url = book_url
     universal_product_code = soup.find_all("td")[0].text
     title = soup.find("h1").text
@@ -35,17 +36,15 @@ def get_book_infos(book_url):
     review_rating = soup.find("p", {"class":"star-rating"})['class'][-1]
     image_url = "http://books.toscrape.com/" + soup.find("img")['src'].split('/',2)[-1]
 
-    product_infos_type = [product_page_url, universal_product_code, title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url]
-
-    return product_infos_type
+    return product_page_url, universal_product_code, title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url
 
 def write_csv():
     en_tete = ["product_page_url", "universal_product_code", "title", "price_including_tax", "price_excluding_tax", "number_available", "product_description", "category", "review_rating", "image_url"]
 
     with open("books.csv", "w", newline="") as fichier_csv:
-        writer = csv.writer(fichier_csv, delimiter=",")
-        writer.writerow(en_tete)
-        for book in get_all_books_urls():
+        writer = csv.writer(fichier_csv, delimiter=",") ## define the writing method
+        writer.writerow(en_tete) ## write the columns title
+        for book in get_all_books_urls(): ## write the retrieved data in each column
             writer.writerow(get_book_infos(book))
     
 write_csv()
