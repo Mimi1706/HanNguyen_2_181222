1fb4b611cda9e97197d25da8a4533faddd0820f9
Mimi <hana.nguyen@outlook.fr>
2022-12-29 17:07:32 +0100

    Updated name

--- one_book_scraping.py
+++ one_book_scraping.py
@@ -1,21 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import os
 
-page_url = "" ## paste the page link inside "" example: "http://books.toscrape.com/catalogue/category/books/childrens_11/page-2.html"
-
-def get_books_urls():
-    response = requests.get(page_url)
-    soup = BeautifulSoup(response.text, "html.parser") ## parser type
-
-    book_infos = soup.find_all("article", {"class": "product_pod"})
-    books_urls = []
-
-    for book in book_infos:
-        book_url = "http://books.toscrape.com/catalogue/" + '/'.join(book.find("a")['href'].split('/')[3:]) ## url of the book
-        books_urls.append(book_url)
-    
-    return books_urls
+book_url = "" ## paste the book link inside "" example: "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 
 def get_book_infos(book_url):
     response = requests.get(book_url) ## gets to the book url
@@ -24,7 +12,7 @@ def get_book_infos(book_url):
     ## retrieves the following data
     product_page_url = book_url
     universal_product_code = soup.find_all("td")[0].text
-    title = soup.find("h1").text
+    title = ' '.join(e for e in soup.find("h1").text.lower().split(' ') if e.isalnum())
     price_including_tax = soup.find_all("td")[3].text
     price_excluding_tax = soup.find_all("td")[2].text
     number_available = soup.find_all("td")[5].text
@@ -33,15 +21,25 @@ def get_book_infos(book_url):
     review_rating = soup.find("p", {"class":"star-rating"})['class'][-1]
     image_url = "http://books.toscrape.com/" + soup.find("img")['src'].split('/',2)[-1]
 
+    ##.lower().split(' ') if e.isalnum())
+
     return product_page_url, universal_product_code, title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url
 
-def write_csv():
+def get_book():
     en_tete = ["product_page_url", "universal_product_code", "title", "price_including_tax", "price_excluding_tax", "number_available", "product_description", "category", "review_rating", "image_url"]
 
-    with open("books-to-scrape.csv", "w", newline="") as fichier_csv:
+    book_title = '_'.join(get_book_infos(book_url)[2].split(' '))
+
+    if not os.path.exists("./{0}".format(book_title)):
+        os.makedirs("./{0}".format(book_title)) ## creates the category folder
+
+    with open("{0}/{0}.csv".format(book_title), "w", newline="") as fichier_csv:
         writer = csv.writer(fichier_csv, delimiter=",") ## defines the writing method
         writer.writerow(en_tete) ## writes the columns title
-        for book in get_books_urls(): ## writes the retrieved data in each column
-            writer.writerow(get_book_infos(book))
+        writer.writerow(get_book_infos(book_url))
     
-write_csv()
+    img_data = requests.get(get_book_infos(book_url)[-1]).content ## retrieves the image content via the url
+    with open('./{0}/{0}.jpg'.format(book_title), 'wb') as image_file:
+        image_file.write(img_data) ## saves the image
+    
+get_book()
