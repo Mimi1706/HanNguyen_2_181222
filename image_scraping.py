acf877f5f78660dd7b8217eaf488e2096d10e629
Mimi <hana.nguyen@outlook.fr>
2022-12-29 12:28:00 +0100

    Image scraping completed and working

---
+++ image_scraping.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+import os ## to create a directory
+
+book_url = "http://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html" ## paste the book link inside "" example: "http://books.toscrape.com/catalogue/the-cat-in-the-hat-beginner-books-b-1_235/index.html"
+
+def get_image_url(book_url):
+    response = requests.get(book_url) ## get to the book url
+    soup = BeautifulSoup(response.text, "html.parser") 
+    ## retrieve the image
+    image_url = "http://books.toscrape.com/" + soup.find("img")['src'].split('/',2)[-1]
+    title = '_'.join(soup.find("h1").text.lower().split(' '))
+
+    return image_url, title
+
+def save_image(image_url, title):
+    if not os.path.exists("./images"):
+        os.makedirs("./images") ## makes books folder
+
+    img_data = requests.get(image_url).content
+    with open('./images/{0}.jpg'.format(title), 'wb') as handler:
+        handler.write(img_data)
+
+save_image(get_image_url(book_url)[0], get_image_url(book_url)[1])
